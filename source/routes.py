a2ce6af99ce17aa61b537fb27b5193b8ad878349
noobg1 <jeevu.art@gmail.com>
2016-12-31 08:19:42 +0530

    async task feature added

--- source/routes.py
+++ source/routes.py
@@ -1,5 +1,6 @@
 import os
 import json
+import csv
 import codecs
 import logging
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
@@ -15,10 +16,14 @@ from flask import render_template, request, flash, session, url_for, redirect
 from forms import ContactForm, SignupForm, SigninForm
 from flask_mail import Message, Mail
 from models import db, User
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
 
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+executor = ThreadPoolExecutor(1)
 
-app.config['ALLOWED_EXTENSIONS'] = set(['html', 'pdf','PROJECT','log'])
+app.config['UPLOAD_FOLDER'] = 'source/uploads/'
+
+app.config['ALLOWED_EXTENSIONS'] = set(['html', 'csv'])
 
 filename = ""
 
@@ -31,6 +36,9 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 glistname = ""
+
+
+
 def getlistname():
   return glistname
 
@@ -44,6 +52,79 @@ def sendMail(name,mailid,subject,content):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/confirm_unsubscribe',methods=['POST'])
+def confirm_unsubscribe():
+  unsub_list = request.form.getlist('check')
+  print unsub_list
+  encoded_un_list = []
+  for un in unsub_list:
+    encoded_un_list.append(eval(un.encode('UTF8')))
+   
+  for n_m in encoded_un_list:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = "DELETE FROM category_subscriber WHERE subname = "+ str("'"+n_m[0]+"'") +" AND submailid = " + str("'"+n_m[1]+"'") +" AND cscampaignermailid = " + str("'"+session['email']+"'") +";"
+    print query
+    try:
+      cursor.execute(query)
+    except: pass
+    query = "DELETE FROM subscriber WHERE sname = " + str("'"+n_m[0]+"'") +" AND semailid = " + str("'"+n_m[1]+"'") +";"
+    print query
+    try:
+      cursor.execute(query)
+    except: pass
+    query = "INSERT INTO unsubscribers (usname,usmailid,uncampaigner_mailid,untime) VALUES (" + str("'"+n_m[0]+"'") + "," + str("'"+n_m[1]+"'") + ","  + str("'"+session['email']+"'") +",NOW());"
+    print query
+    try:
+      cursor.execute(query)
+    except: pass
+
+  conn.commit()
+  conn.close()
+
+  print encoded_un_list
+  return str("got list")
+
+@app.route('/unsubscribe',methods=['POST','GET'])
+def unsubscirbe_from_table_selection():
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+    if request.method == 'GET':   
+      contacts = request.args.get('editor1')
+      conn = mysql.connect()
+      cursor = conn.cursor()
+      query = "SELECT DISTINCT(catname) FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+";"
+      cursor.execute(query)
+      data = cursor.fetchall()
+      datalist = []
+      for t in data:
+        item = [item.encode('utf-8') for item in t]
+        datalist.append(item[0])
+      conn.close();
+      return render_template('unsubscribe.html',datalist=datalist)
+
+    elif request.method == 'POST':
+      cat = request.form['category']
+      try:
+        conn = mysql.connect()
+        cursor = conn.cursor() 
+        query = "SELECT subname,submailid FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+" AND catname = " + str("'"+cat+"'")+";"
+        print query
+        cursor.execute(query)
+        data = cursor.fetchall()
+        conn.close()
+        fullist = []
+        for t in data:
+          fullist.append([item.encode('utf-8') for item in t])
+        
+        
+        return render_template('unsubscribe.html',fullist=fullist)
+      except:
+        return redirect(url_for('error'))
+
+  
+
 
 @app.route('/subscribers/<name_mailid>',methods=['POST','GET'])
 def subscribers(name_mailid):
@@ -65,23 +146,27 @@ def updatelist(listname):
         name_mailid = [item.encode('utf-8') for item in name_mailid]
         conn = mysql.connect()
         cursor = conn.cursor() 
-        query = ""
-        querycat = "INSERT INTO category (cname,ctime) VALUES (" + str('"'+listname+'"') +",NOW()); "
-        query += querycat
-        print querycat
+        
+        query = "INSERT INTO category (cname,ctime) VALUES (" + str('"'+listname+'"') +",NOW()); "
+        
+        try:
+            cursor.execute(query)
+        except: pass
+        
         
         for i in name_mailid:
           i = i.split(',')
-          querysub = "INSERT INTO subscriber (sname,semailid,stime) VALUES (" + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"')+",NOW());"
-          query += querysub
-          print querysub
+          query = "INSERT INTO subscriber (sname,semailid,stime) VALUES (" + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"')+",NOW());"
+          try:
+            cursor.execute(query)
+          except: pass
           
-          querysub_cat = "INSERT INTO category_subscriber (catname,subname,submailid,cscampaignermailid,cstime) VALUES (" + str('"'+listname+'"') + ","  + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"') +"," + str('"'+str(session['email'])+'"') + ",NOW());"
-          query += querysub_cat
-          print querysub_cat
+          query = "INSERT INTO category_subscriber (catname,subname,submailid,cscampaignermailid,cstime) VALUES (" + str('"'+listname+'"') + ","  + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"') +"," + str('"'+str(session['email'])+'"') + ",NOW());"
+          try:
+            cursor.execute(query)
+          except: pass
 
-        print query
-        cursor.execute(query)
+        
         conn.commit()
         conn.close()
         print listname
@@ -92,9 +177,11 @@ def updatelist(listname):
 
          
       except:
+
         return redirect(url_for('error'))
   elif request.method == 'GET':
     listname = glistname
+    print "here"
     return render_template('updatelist.html',listname = listname)
 
   return render_template('newlist.html')
@@ -120,49 +207,256 @@ def newlist():
 
   return render_template('newlist.html')
 
-@app.route('/list',methods=['POST','GET'])
-def list():
+@app.route('/list/<cat>',methods=['GET'])
+def listpost(cat):
+  try:
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    query = "SELECT subname,submailid FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+" AND catname = " + str("'"+cat+"'")+";"
+    print query
+    cursor.execute(query)
+    data = cursor.fetchall()
+    conn.close()
+    datalist = []
+    for t in data:
+      datalist.append([item.encode('utf-8') for item in t])
+    print datalist
+    
+    return render_template('subscriberslist.html',datalist=datalist)
+  except:
+    return redirect(url_for('error'))
+
+@app.route('/list',methods=['GET','POST'])
+def catlist():
     if 'email' not in session:
       return redirect(url_for('signin'))
 
-    # conn = mysql.connect()
-    # cursor = conn.cursor() 
-    
     if request.method == 'POST':
-        try:
-          print "test"
-            # query = ""
-            # cursor.execute(query)
-            # data = cursor.fetchall()
-            # valstr = []
-            # for a in data:     
-            #   valstr += a     
-            # attributes = valstr  
-            # print data
-            # conn.close() 
-            # return str(data)
-          return render_template('newlist.html')
-        except:
-          return redirect(url_for('error'))
+      return render_template('newlist.html')
+
+    elif request.method == 'GET':  
+      try:
+        conn = mysql.connect()
+        cursor = conn.cursor() 
+        query = "SELECT DISTINCT(catname) FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+";"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        datalist = []
+        for t in data:
+          item = [item.encode('utf-8') for item in t]
+          datalist.append(item[0])
+
+
+        conn.close();
+        print str(data),type(data)
+        return render_template('list.html',datalist=datalist)
+        
+      except:
+        return redirect(url_for('error'))
 
     return render_template('list.html')
+
+@app.route('/editcampaign/<subject>',methods=['GET','POST'])
+def editcampaign(subject):
+    if request.method == 'GET':
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            query = "SELECT cpcategory,cpcontent FROM campaign WHERE cpsubject = " + str("'"+subject+"'") + ";"
+            cursor.execute(query)
+            data = cursor.fetchall()
+            datal = list(data)
+            datalist = []
+            for d in datal:
+              datalist.append(list(d))
+            print datalist
+
+            query = "SELECT DISTINCT(catname) FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+";"
+            cursor.execute(query)
+            data = cursor.fetchall()
+            catlist = []
+            for t in data:
+              item = [item.encode('utf-8') for item in t]
+              catlist.append(item[0])
+
+            conn.close()
+            return render_template('editcampaign.html',datalist=datalist[0],catlist=catlist)
+        except Exception as e:
+          print e
+          return redirect(url_for('error'))
+    elif request.method == 'POST':
+        cat = request.form['option']
+        content = request.form['editor1']
+        subject = request.form['subject']
+        conn = mysql.connect()
+        cursor = conn.cursor() 
+        query = "SELECT subname,submailid FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+" AND catname = " + str("'"+cat+"'")+";"
+        
+        try:
+          cursor.execute(query)
+          data = cursor.fetchall()
+        except Exception as e:
+          print e
+        try:
+          query = "INSERT INTO campaign (cpcampaignermailid,cpsubject,cpcategory,cpcontent,cptime) VALUES (" + str("'"+session['email']+"'")+ "," + str("'"+subject+"'") + "," + str("'"+cat+"'") +  "," + str("'"+content+"'") +",NOW());"
+          cursor.execute(query)
+        except Exception as e:
+          print e
+        conn.commit()
+        conn.close()
+        datalist = []
+        for t in data:
+          datalist.append([item.encode('utf-8') for item in t])
+        for nm in datalist:
+          print nm[0],nm[1]
+          try:
+            sendMail(nm[0],[nm[1]],subject,content)
+          except Exception as e:
+            print e
+        
+        return str(content)
+        return redirect(url_for('newcampaign'))
+
+
+@app.route('/allcampaigns')
+def allcampaigns():
+  if 'email' not in session:
+      return redirect(url_for('signin'))
+  try:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = "SELECT * FROM `campaign` WHERE  cpcampaignermailid = " + str("'"+session['email']+"'")+";"
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    datal = list(data)
+    flist = []
+    for d in datal:
+      flist.append(list(d))
+    print flist
+    return render_template('allcampaigns.html',flist=flist)
+    
+  except Exception as e:
+    print e
+    return redirect(url_for('error'))
+
+
 
 @app.route('/newcampaign')
 def newcampaign():
     if 'email' not in session:
       return redirect(url_for('signin'))
     contacts = request.args.get('editor1')
-    print "jhgfudsf",contacts
-    return render_template('newcampaign.html')
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = "SELECT DISTINCT(catname) FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+";"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    datalist = []
+    for t in data:
+      item = [item.encode('utf-8') for item in t]
+      datalist.append(item[0])
+    conn.close();
+
+    return render_template('newcampaign.html',datalist=datalist)
 
 @app.route('/newcampaign', methods = ['POST'])
 def accept_campaign():
-    name = request.form['name']
-    mailid = request.form['mailid']
+    # name = request.form['name']
+    # mailid = request.form['mailid']
+    # sendMail(name,mailid.split(","),subject,content)
+    cat = request.form['option']
     content = request.form['editor1']
     subject = request.form['subject']
-    sendMail(name,mailid.split(","),subject,content)
-    return str(content)
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    query = "SELECT subname,submailid FROM `category_subscriber` WHERE cscampaignermailid = " + str("'"+session['email']+"'")+" AND catname = " + str("'"+cat+"'")+";"
+    
+    try:
+      cursor.execute(query)
+      data = cursor.fetchall()
+    except Exception as e:
+      print e
+    try:
+      query = "INSERT INTO campaign (cpcampaignermailid,cpsubject,cpcategory,cpcontent,cptime) VALUES (" + str("'"+session['email']+"'")+ "," + str("'"+subject+"'") + "," + str("'"+cat+"'") +  "," + str("'"+content+"'") +",NOW());"
+      cursor.execute(query)
+    except Exception as e:
+      print e
+    conn.commit()
+    conn.close()
+    datalist = []
+    for t in data:
+      datalist.append([item.encode('utf-8') for item in t])
+    for nm in datalist:
+      print nm[0],nm[1]
+      try:
+        
+        executor.submit(sendMail,nm[0],[nm[1]],subject,content)
+        
+      except Exception as e:
+        print e
+    
+    return render_template('index.html')
+
+@app.route('/updatelistbyfile', methods=['POST'])
+def updatelistbyfile():
+    global filename
+    global glistname
+    # Get the name of the uploaded file
+    file = request.files['file']
+    print file
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        
+        filename = secure_filename(file.filename)
+        
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        print "type:",(filename)
+        try:
+          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        except: print "failed to save"
+
+        f = open('source/uploads/'+ filename, 'rb') # opens the csv file
+        
+        
+        
+         
+
+        conn = mysql.connect()
+        cursor = conn.cursor() 
+        query = ""
+        listname = glistname
+        print listname
+        query = "INSERT INTO category (cname,ctime) VALUES (" + str('"'+listname+'"') +",NOW()); "
+        
+        try:
+            cursor.execute(query)
+        except: pass
+        
+        reader = csv.reader(f)
+        for i in reader:
+          
+          query = "INSERT INTO subscriber (sname,semailid,stime) VALUES (" + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"')+",NOW());"
+          try:
+            cursor.execute(query)
+          except: pass
+          
+          query = "INSERT INTO category_subscriber (catname,subname,submailid,cscampaignermailid,cstime) VALUES (" + str('"'+listname+'"') + ","  + str('"'+i[0]+'"') + "," + str('"'+i[1]+'"') +"," + str('"'+str(session['email'])+'"') + ",NOW());"
+          try:
+            cursor.execute(query)
+          except: pass
+
+        
+        f.close()
+        
+        conn.commit()
+        conn.close() 
+
+        return str("uploaded")   
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
